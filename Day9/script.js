67b1e60d23841520d00a6bb761bd8218cce68456
MAHESHPATELD2 <patelmahesh3875@gmail.com>
2025-07-09 22:05:49 +0530

    Create script.js

---
+++ Day9/script.js
@@ -0,0 +1,43 @@
+//variables
+let name="mahesh";
+console.log(name);
+const pi=3.14;
+console.log(pi);
+var age=20;
+console.log(age);
+//type of variables
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(pi))
+//logical operator
+let a=true;
+let b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+//comparision operator
+console.log(5=="5");
+console.log(5==="5");
+console.log(10>5);
+//arithimatic operator
+let result1=10+5;
+let result2=10-5;
+let result3=10*5;
+let result4=10/5;
+let result5=10%5;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+//ternary operator
+let ages=18;
+let status=(ages>=18)?"Adult":"Minor";
+console.log(status);
+//assignment operator
+let x=10;
+console.log(x+=5);
+console.log(x-=5);
+console.log(x)
+console.log(x)
+console.log(x)
